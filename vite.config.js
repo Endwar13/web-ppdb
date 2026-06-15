ae88cf0e91ec300aa08729a817766f6677b21890
Endwar13 <khrisnatirtaendira@gmail.com>
2026-06-15 09:26:18 +0700

    dark-repair

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path';
+import tailwindcss from '@tailwindcss/vite'//import file tailwind
+import { resolve } from 'path';//import file html lain dari node js
 
 export default defineConfig({
   plugins: [
@@ -9,9 +9,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        fasilitas: resolve(__dirname, 'Fasilitas.html'),
-        program: resolve(__dirname, 'program.html'),
+        main: resolve(__dirname, 'index.html'),//Untuk Halaman Utama
+        fasilitas: resolve(__dirname, 'Fasilitas.html'),//Halaman lain 
+        program: resolve(__dirname, 'program.html'),//Halaman Lain
       },
     },
   },
